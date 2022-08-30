79e7a3b7c06602041297415e3d4dd96514092614
Ryan Djemtchemeu <ryanmambou@gmail.com>
2022-08-30 10:08:09 +0100

    responsive nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,10 +3,17 @@ import { NavLink } from 'react-router-dom';
 import Search from './Search';
 import Notification from './Notification';
 import User from './User';
-import styles from '../styles/nav.module.css'
+import styles from '../styles/nav.module.css';
+import { AiFillCaretDown } from "react-icons/ai";
+import Tippy from '@tippyjs/react';
 
 function Nav({links}) {
   const [color, setColor] = useState(false);
+  const [caret, setCaret] = useState(false);
+
+  const rotateCaret = () => {
+    setCaret(!caret)
+  }
 
   const changeColor = () => {
     if (window.scrollY >= 30){
@@ -28,6 +35,20 @@ function Nav({links}) {
                 <NavLink key={index} to='/' className={styles.link}>{link}</NavLink>
             ))}
             </div>
+            <Tippy interactive={true} placement={'bottom'} trigger='click'
+            content={
+              links.map((link, index) => (
+                <div className={styles.links_tippy}>
+                <NavLink key={index} to='/' className={styles.link_tippy}>{link}</NavLink>
+                </div>
+            ))
+            }
+            >
+            <span className={`${styles.mobile_nav}`} onClick={rotateCaret}>
+            <span>Parcourir</span>
+              < AiFillCaretDown className={`${styles.caret} ${caret ? styles.rotate: ''}`}/>
+              </span>
+              </Tippy>
         </div>
         <div className={styles.right}>
             <span><Search /></span>
